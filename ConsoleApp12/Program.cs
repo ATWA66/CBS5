ea0a1e8a38aa66d9e11de2a47b079634e58d70b9
ATWA66 <98480275+ATWA66@users.noreply.github.com>
2022-08-09 13:54:20 +0300

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿//task 5
+int myValue = 32;
+while (1>0)
+{
+    if (myValue %2 == 0)
+    {
+        myValue /= 2;
+        if(myValue == 1)
+        {
+            Console.WriteLine("число является степенью 2");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("число не является степенью 2");
+        break;
+    }
+ 
+}
